28fe0d22df56b1b46f6a79ee3bdbad37061cdfc8
netmanchris <chris_young@me.com>
2018-10-23 15:23:29 -0400

    Added GetStatus Test

--- tests/test_pybrid_data.py
+++ tests/test_pybrid_data.py
@@ -2,5 +2,17 @@ from unittest import TestCase
 from pybrid.auth import *
 from pybrid.data import *
 
-my_brid = '10.101.0.124'
+auth = BridAuth('10.101.0.124')
 
+class TestGetStatus(TestCase):
+    """
+    Test Case for pyawair.data get_current_air_data function
+    """
+
+    def test_get_current_air(self):
+        """
+        """
+        current_status = get_status(auth)
+        data_points = ['Time', 'Sensors', 'Settings', 'Filters']
+        for data_point in current_status.keys():
+            self.assertIn(data_point, data_points)
